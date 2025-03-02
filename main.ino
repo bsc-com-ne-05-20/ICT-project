dc8e0e33b8ce3fb227f7225406ce206e8676fc12
Tadala Sumani <bsc-com-ne-09-19@unima.ac.mw>
2025-03-02 17:37:20 +0200

    created configurations variables

---
+++ main.ino
@@ -0,0 +1,10 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+// Wi-Fi credentials
+const char* ssid = "YOUR_WIFI_SSID";       
+const char* password = "YOUR_WIFI_PASSWORD";
+
+// ThingSpeak API Key
+const char* server = "api.thingspeak.com";
+String apiKey = "YOUR_THINGSPEAK_API_KEY";
