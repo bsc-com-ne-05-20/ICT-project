013dc880d58fe857599aea6e4033bdbec3302fb2
Tadala Sumani <bsc-com-ne-09-19@unima.ac.mw>
2025-03-03 15:31:32 +0200

    added calibration values

--- main.ino
+++ main.ino
@@ -3,14 +3,20 @@
 
 // Wi-Fi credentials
 const char* ssid = "";       
-const char* password = "YOUR_WIFI_PASSWORD";
+const char* password = "";
 
 // ThingSpeak API Key
 const char* server = "api.thingspeak.com";
-String apiKey = "YOUR_THINGSPEAK_API_KEY";
+String apiKey = "";
 
 // Pin definitions
 #define SALINITY_SENSOR_PIN 33    // GPIO33 (Analog Input)
 #define PH_SENSOR_PIN 32          // GPIO32 (Analog Input)
 #define LEAD_SENSOR_PIN 35        // GPIO35 (Analog Input)
 #define MERCURY_SENSOR_PIN 34     // GPIO34 (Analog Input)
+
+// Calibration values 
+#define SALINITY_CALIBRATION 0.5  
+#define PH_CALIBRATION 3.0        
+#define LEAD_CALIBRATION 0.05     
+#define MERCURY_CALIBRATION 0.02 
