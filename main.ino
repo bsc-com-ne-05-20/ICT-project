2669ad60c713de778b2fb3b19c486617478511d4
Tadala Sumani <bsc-com-ne-09-19@unima.ac.mw>
2025-03-03 15:22:37 +0200

    defining analog pins where the sensors are connected

--- main.ino
+++ main.ino
@@ -2,9 +2,15 @@
 #include <HTTPClient.h>
 
 // Wi-Fi credentials
-const char* ssid = "YOUR_WIFI_SSID";       
+const char* ssid = "";       
 const char* password = "YOUR_WIFI_PASSWORD";
 
 // ThingSpeak API Key
 const char* server = "api.thingspeak.com";
 String apiKey = "YOUR_THINGSPEAK_API_KEY";
+
+// Pin definitions
+#define SALINITY_SENSOR_PIN 33    // GPIO33 (Analog Input)
+#define PH_SENSOR_PIN 32          // GPIO32 (Analog Input)
+#define LEAD_SENSOR_PIN 35        // GPIO35 (Analog Input)
+#define MERCURY_SENSOR_PIN 34     // GPIO34 (Analog Input)
