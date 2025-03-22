49504f3bda9cee057252af1173037541f066b3a8
Tadala Sumani <bsc-com-ne-09-19@unima.ac.mw>
2025-03-22 21:42:17 +0200

    bluetooth definitions

--- main.ino
+++ main.ino
@@ -4,3 +4,11 @@
 #include <ModbusMaster.h>
 #include <TinyGPS++.h>
 #include <HardwareSerial.h>
+
+// BLE Definitions
+BLEServer* pServer;
+BLEService* pService;
+BLECharacteristic* pDataCharacteristic;
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define DATA_UUID           "beb5483e-36e1-4688-b7f5-ea07361b26a8"
