401ae0506cbc574644d394a2a2b7e8b34817c1d5
Tadala Sumani <bsc-com-ne-09-19@unima.ac.mw>
2025-03-22 21:48:34 +0200

    added modbus configuration

--- main.ino
+++ main.ino
@@ -12,3 +12,8 @@ BLECharacteristic* pDataCharacteristic;
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define DATA_UUID           "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+// RS485 Modbus Configuration
+ModbusMaster node;
+#define MAX485_DE_RE 23     
+#define RS485_BAUD 9600
