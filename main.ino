1f94b446c2509e08e033c542bf89aa10bfb47e23
Tadala Sumani <bsc-com-ne-09-19@unima.ac.mw>
2025-03-22 22:20:04 +0200

    added a callback class to update deviceConnected when a device connects or disconnects.

--- main.ino
+++ main.ino
@@ -13,7 +13,23 @@ BLECharacteristic* pDataCharacteristic;
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define DATA_UUID           "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+// BLE Connection Tracking
+bool deviceConnected = false;
+class ServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) { deviceConnected = true; }
+    void onDisconnect(BLEServer* pServer) { deviceConnected = false; }
+};
+
 // RS485 Modbus Configuration
 ModbusMaster node;
 #define MAX485_DE_RE 23     
 #define RS485_BAUD 9600
+
+// GPS Configuration
+HardwareSerial gpsSerial(1); // UART1 for SIM7000G
+TinyGPSPlus gps;
+#define GPS_BAUD 115200
+
+// Sensor Pins (Analog)
+#define MOISTURE_PIN 34
+#define SALINITY_PIN 35
