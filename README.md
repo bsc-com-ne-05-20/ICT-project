a938ada845b822c83eb99677c6cf42b9aaebf16e
Tadala Sumani <bsc-com-ne-09-19@unima.ac.mw>
2025-03-22 21:36:44 +0200

    defining libaries needed for the project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ICT-project
+[env:esp32dev]
+platform = espressif32
+board = esp32dev
+framework = arduino
+monitor_speed = 115200
+lib_deps =
+    paulstoffregen/TinyGPSPlus
+    4-20ma/ModbusMaster
+    nkolban/ESP32 BLE Arduino
+
--- main.ino
+++ main.ino
@@ -1,61 +1,6 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
-
-// Wi-Fi credentials
-const char* ssid = "";       
-const char* password = "";
-
-// ThingSpeak API Key
-const char* server = "api.thingspeak.com";
-String apiKey = "";
-
-// Pin definitions
-#define SALINITY_SENSOR_PIN 33    // GPIO33 (Analog Input)
-#define PH_SENSOR_PIN 32          // GPIO32 (Analog Input)
-#define LEAD_SENSOR_PIN 35        // GPIO35 (Analog Input)
-#define MERCURY_SENSOR_PIN 34     // GPIO34 (Analog Input)
-
-// Calibration values 
-#define SALINITY_CALIBRATION 0.5  
-#define PH_CALIBRATION 3.0        
-#define LEAD_CALIBRATION 0.05     
-#define MERCURY_CALIBRATION 0.02 
-
-void setup() {
-    Serial.begin(115200);
-   // Serial.println("\nInitializing system...");
-  
-    // Initialize WiFi
-    WiFi.begin(ssid, password);
-    Serial.print("Connecting to WiFi");
-    int wifiTimeout = 0;
-    while (WiFi.status() != WL_CONNECTED && wifiTimeout < 20) { // 20-second timeout
-      delay(500);
-      Serial.print(".");
-      wifiTimeout++;
-    }
-
-    if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("\nConnected to WiFi!");
-        Serial.print("IP Address: ");
-        Serial.println(WiFi.localIP());
-      } else {
-        Serial.println("\nFailed to connect to WiFi!");
-      }
-    }
-    
-    void loop() {
-      if (WiFi.status() != WL_CONNECTED) {
-        Serial.println("WiFi disconnected! Attempting reconnection...");
-        WiFi.reconnect();
-        delay(5000);
-        return;
-      }
-
-      // Read sensors
-    float salinity = readSalinity();
-    float pH = readPH();
-    float lead = readLead();
-    float mercury = readMercury();
-
-      
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <ModbusMaster.h>
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
