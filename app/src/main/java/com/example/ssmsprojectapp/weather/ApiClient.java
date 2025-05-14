514275c21387aaed1e6bf0182174d87c9e1cd678
bsc_com_ne_05_20 <bsc_com_ne_05_20@unima.ac.mw>
2025-05-14 08:28:31 +0200

    added openWeatherMap API client
    Signed-off-by: bsc_com_ne_05_20 <bsc_com_ne_05_20@unima.ac.mw>

---
+++ app/src/main/java/com/example/ssmsprojectapp/weather/ApiClient.java
@@ -0,0 +1,29 @@
+package com.example.ssmsprojectapp.weather;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
+    private static Retrofit retrofit = null;
+
+    public static WeatherApiService getApiService() {
+        if (retrofit == null) {
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(loggingInterceptor)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(WeatherApiService.class);
+    }
+}
