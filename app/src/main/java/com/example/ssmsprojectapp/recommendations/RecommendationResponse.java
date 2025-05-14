341a7fa65742cf19227b8214e1a188722e035f19
bsc_com_ne_05_20 <bsc_com_ne_05_20@unima.ac.mw>
2025-05-14 13:11:50 +0200

    added the agrichatbot Recommendation response model
    Signed-off-by: bsc_com_ne_05_20 <bsc_com_ne_05_20@unima.ac.mw>

---
+++ app/src/main/java/com/example/ssmsprojectapp/recommendations/RecommendationResponse.java
@@ -0,0 +1,16 @@
+package com.example.ssmsprojectapp.recommendations;
+
+public class RecommendationResponse {
+
+    private String recommendations;
+    // Getter and setter
+
+
+    public String getRecommendations() {
+        return recommendations;
+    }
+
+    public void setRecommendations(String recommendations) {
+        this.recommendations = recommendations;
+    }
+}
