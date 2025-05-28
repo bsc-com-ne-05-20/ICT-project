adae452ae5565e745e2476983f56cf1bb9dd149d
bsc-com-ne-09-19 <bsc-com-ne-09-19@unima.ac.mw>
2025-05-28 12:17:47 +0200

    Added Bluetooth and location permission-checking logic

--- app/src/main/java/com/example/ssmsprojectapp/sensor_data_receiver/controller/MainController.java
+++ app/src/main/java/com/example/ssmsprojectapp/sensor_data_receiver/controller/MainController.java
@@ -57,4 +57,37 @@ public class MainController {
         view.getClearButton().setOnClickListener(v -> view.clearSensorData());
         view.getDisconnectButton().setOnClickListener(v -> disconnect());
     }
+
+    private void checkPermissions() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(
+                    (android.app.Activity) context,
+                    new String[]{
+                            Manifest.permission.BLUETOOTH_SCAN,
+                            Manifest.permission.BLUETOOTH_CONNECT,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    101);
+        }
+    } else {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(
+                    (android.app.Activity) context,
+                    new String[]{
+                            Manifest.permission.BLUETOOTH,
+                            Manifest.permission.BLUETOOTH_ADMIN,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    101);
+        }
+    }
+}
+
 }
