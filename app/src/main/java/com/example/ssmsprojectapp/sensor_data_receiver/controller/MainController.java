95fefcd222ce5008ceea502ce2956a417793596d
bsc-com-ne-09-19 <bsc-com-ne-09-19@unima.ac.mw>
2025-05-28 12:03:26 +0200

    Created MainController.java with initial class and imports

---
+++ app/src/main/java/com/example/ssmsprojectapp/sensor_data_receiver/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.example.ssmsprojectapp.sensor_data_receiver.controller;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.bluetooth.*;
+import android.content.*;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+
+import com.example.ssmsprojectapp.sensor_data_receiver.model.SensorData;
+import com.example.ssmsprojectapp.sensor_data_receiver.view.MainView;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class MainController {
+}
